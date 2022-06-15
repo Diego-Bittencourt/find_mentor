2a8bec64ee3e97eca038aa30087ac95dc63ef8d7
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-06-15 22:59:40 +0900

    FEAT - implement the form. The form sends the data through a method.

--- src/pages/mentors/MentorRegistration.vue
+++ src/pages/mentors/MentorRegistration.vue
@@ -1,3 +1,17 @@
 <template>
-    Register
+    <section>
+        <base-card>
+        <h2>Register as a Mentor now!</h2>
+        <mentor-form></mentor-form>
+        </base-card>
+        </section>
 </template>
+
+<script>
+import MentorForm from '../../components/mentors/MentorForm.vue';
+export default {
+    components: {
+        MentorForm
+    }
+}
+</script>
