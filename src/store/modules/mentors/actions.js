5c95f7d1dd3cdaaa973d36b0bb2205eb56229780
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-06-19 12:58:58 +0900

    FEAT - fetching data to the firebase
    I created a PUT fetch to send register as a mentor data.
    In the firebase, each user will have his/her own info. The PUT method
    evaluates and updates if exists data. The POST method always creates
    new data.

--- src/store/modules/mentors/actions.js
+++ src/store/modules/mentors/actions.js
@@ -1,13 +1,29 @@
 export default {
-    registerMentor(context, payload) {
+    async registerMentor(context, payload) {
+        const userId = context.rootGetters.userId;
         const mentorData = {
-            id: context.rootGetters.userId,
             firstName: payload.first,
             lastName: payload.last,
             description: payload.description,
             hourlyRate: payload.rate,
             areas: payload.areas
         };
-        context.commit('registerMentor', mentorData);
+
+        const response = await fetch(`https://mentors-fcf7f-default-rtdb.asia-southeast1.firebasedatabase.app/mentors/${userId}.json`,
+        {
+            method: 'PUT',
+            body: JSON.stringify(mentorData)
+        });
+
+        // const responseData = await response.json();
+
+        if(!response.ok) {
+            // error ...
+        } 
+        
+        context.commit('registerMentor', {
+            ...mentorData,
+            id: userId
+        });
     }
 };
